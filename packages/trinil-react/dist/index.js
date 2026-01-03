86b8aa8c3de611239ed86c91dffdc4311a6f3f60
Rémi Courtillon <remi.courtillon@gmail.com>
2026-01-03 18:16:53 +0100

    chore(release): v1.3.5

--- packages/trinil-react/dist/index.js
+++ packages/trinil-react/dist/index.js
@@ -9111,7 +9111,7 @@ import React1013 from "react";
 import { jsx as jsx1013 } from "react/jsx-runtime";
 var VideoOff = React1013.memo((props) => {
   const { size = 24, color = "currentColor", strokeWidth = 1.5, className, title, ariaLabel } = props;
-  return /* @__PURE__ */ jsx1013("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 24 24", fill: "none", stroke: color, strokeWidth, strokeLinecap: "round", strokeLinejoin: "round", vectorEffect: "non-scaling-stroke", width: size, height: size, className, role: "img", "aria-label": ariaLabel, dangerouslySetInnerHTML: { __html: `<path d="M0 0h24v24H0z"/><path d="M14.25 8.25v-1.5a1.5 1.5 0 0 0-1.5-1.5h-3m4.5 3 7.5-3v12m-7.5-9v1.5m0 6v1.5a1.5 1.5 0 0 1-1.5 1.5h-9a1.5 1.5 0 0 1-1.5-1.5V6.75a1.5 1.5 0 0 1 1.5-1.5h1.5m9 10.5v-1.5m0 1.5 2.5 1M5.25 5.25l-3-3m3 3L7.5 7.5l2.25 2.25L12 12l2.25 2.25m0 0 2.5 2.5m0 0 5 5"/>` }, children: title && /* @__PURE__ */ jsx1013("title", { children: title }) });
+  return /* @__PURE__ */ jsx1013("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 24 24", fill: "none", stroke: color, strokeWidth, strokeLinecap: "round", strokeLinejoin: "round", vectorEffect: "non-scaling-stroke", width: size, height: size, className, role: "img", "aria-label": ariaLabel, dangerouslySetInnerHTML: { __html: `<path d="M14.25 8.25v-1.5a1.5 1.5 0 0 0-1.5-1.5h-3m4.5 3 7.5-3v12m-7.5-9v1.5m0 6v1.5a1.5 1.5 0 0 1-1.5 1.5h-9a1.5 1.5 0 0 1-1.5-1.5V6.75a1.5 1.5 0 0 1 1.5-1.5h1.5m9 10.5v-1.5m0 1.5 2.5 1M5.25 5.25l-3-3m3 3L7.5 7.5l2.25 2.25L12 12l2.25 2.25m0 0 2.5 2.5m0 0 5 5"/>` }, children: title && /* @__PURE__ */ jsx1013("title", { children: title }) });
 });
 VideoOff.displayName = "VideoOff";
 
